da4c65ea1a7b3548d71a38d9a05630077b2b6a84
Adriaan Dokter <adriaandokter@gmail.com>
2023-06-09 14:54:47 -0400

    remove stray #'s

--- R/solarnoon.R
+++ R/solarnoon.R
@@ -14,7 +14,7 @@
 #' @param POSIXct.out Logical, if `TRUE`, the result is returned as a `POSIXct` object, otherwise, it is returned as a fraction of a day.
 #' @details
 #' Input can consist of one location and at least one `POSIXct`` time, or one `POSIXct`` time and at least
-#' one location. #' Do not use the daylight savings time zone string for supplying `dateTime`, as many OS will not be
+#' one location. Do not use the daylight savings time zone string for supplying `dateTime`, as many OS will not be
 #' able to properly set it to standard time when needed.
 #' NOAA notes that “for latitudes greater than 72 degrees N and S, calculations are accurate to within
 #' 10 minutes. For latitudes less than +/- 72 degrees accuracy is approximately one minute.”
